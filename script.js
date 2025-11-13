b62be67d66331fac1c8e36a30223bbc30c1094a0
SnehaM27 <snehamohan0227@gmail.com>
2025-11-13 18:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // if substring is empty, return 0
+  if (s2.length === 0) return 0;
+
+  // convert both strings to lowercase for case-insensitive comparison
+  const str = s1.toLowerCase();
+  const sub = s2.toLowerCase();
+
+  return str.indexOf(sub);
 }
 
 // Please do not change the code below
